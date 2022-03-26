95cc5c61c58fb30bec97373450b9a37512262a92
zhong wang <zhongwang07@yahoo.com>
2022-03-26 23:45:09 +0000

    hhhhh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 import { defineNuxtConfig } from 'nuxt3'
-
+import ElementPlus from 'element-plus/dist/index.full'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    css: [
-        '~/assets/scss/main.scss'
-    ]
+
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,35 @@
 <template>
   <h1>Index page</h1>
-  <label for="customRange1" class="form-label">Example range</label>
-  <input type="range" class="form-range" id="customRange1" />
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+  </el-row>
+
+  <el-row class="mb-4">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </el-row>
+
+  <el-row class="mb-4">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </el-row>
 </template>
+<script setup lang="ts">
+function update() {
+  console.log("update");
+}
+</script>
