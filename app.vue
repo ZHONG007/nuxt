bc29815e1a9b404bceae653be4fea92f2601a8a9
zhong wang <zhongwang07@yahoo.com>
2022-03-26 08:47:39 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+
+if (route.params.group === "admins" && !route.params.id) {
+  console.log("Warning! Make sure user is authenticated!");
+}
+</script>
---
+++ pages/user-[group]/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <p>{{ $route.params.group }} - {{ $route.params.id }}</p>
+</template>
+
+<script setup>
+definePageMeta({
+  title: "My home page",
+});
+</script>
