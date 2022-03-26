e5a4ced5a20992ed3539cc28e0e4419c734c8caf
zhong wang <zhongwang07@yahoo.com>
2022-03-26 23:09:47 +0000

    bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 
-    meta: {
-        meta: [
-            // <meta name="viewport" content="width=device-width, initial-scale=1">
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-        ],
-        link: [
-            // <link rel="stylesheet" href="https://myawesome-lib.css">
-            { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' }
-        ],
-        // please note that this is an area that is likely to change
 
-    }
 
 })
