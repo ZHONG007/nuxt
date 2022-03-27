9d2fcd1d1aac9e16b7e1feaf20ae48bdac5809f6
zhong wang <zhongwang07@yahoo.com>
2022-03-27 00:09:12 +0000

    ppppppp

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from '#app'
-import ElementPlus from 'element-plus/dist/index.full'
+import ElementPlus from 'element-plus'
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.use(ElementPlus)
 })
