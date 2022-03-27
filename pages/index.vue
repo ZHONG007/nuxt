89173aefdd427d10e97b2c7cb9b0bffaa4be131d
zhong wang <zhongwang07@yahoo.com>
2022-03-27 00:58:42 +0000

    5555555555555555

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+<p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
   <div style="text-align: center">
     <h1>试试按钮样式</h1>
     <van-button type="primary" @click="updateInput">主要按钮</van-button>
--- pages/user-[group]/[id].vue
+++ pages/user-[group]/[id].vue
@@ -1,5 +1,9 @@
 <template>
   <p>{{ $route.params.group }} - {{ $route.params.id }}</p>
+    <div style="text-align: center">
+    <h1>试试按钮样dsds式</h1>
+    <van-button type="primary" @click="updateInput">主要按钮</van-button>
+  </div>
 </template>
 
 <script setup>
