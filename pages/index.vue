9839b1561c74615e18343dc95b5658d7a7d47260
zhong wang <zhongwang07@yahoo.com>
2022-03-27 00:47:58 +0000

    vant anan

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div style="text-align: center">
     <h1>试试按钮样式</h1>
-    <van-button type="primary">主要按钮</van-button>
+    <van-button type="primary" @click="updateInput">主要按钮</van-button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 const input = ref("");
+function updateInput() {
+  console.log('6666');
+}
 </script>
