943b49198770a3f53439d335cab9542666ee1213
zhong wang <zhongwang07@yahoo.com>
2022-04-11 23:14:53 +0100

    very goooood

--- pages/index.vue
+++ pages/index.vue
@@ -105,18 +105,13 @@
           ></Button>
         </section>
       </div>
-      <div class="col-12 md:col-6 overflow-hidden">
-        <img
-          src="images/blocks/hero/hero-1.png"
-          alt="Image"
-          class="md:ml-auto block md:h-full"
-          style="clip-path: polygon(8% 0, 100% 0%, 100% 100%, 0 100%)"
-        />
-      </div>
     </div>
     <Card>
       <template #header>
-        <img alt="user header" src="https://cdn.pixabay.com/photo/2018/06/17/20/35/chain-3481377_960_720.jpg" />
+        <img
+          alt="user header"
+          src="https://cdn.pixabay.com/photo/2018/06/17/20/35/chain-3481377_960_720.jpg"
+        />
       </template>
       <template #title> Advanced Card </template>
       <template #content>
